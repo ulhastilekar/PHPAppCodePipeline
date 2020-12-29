99a861dee666e3f868d3ec72122a3f39c18292e4
Tilekar <tilekaru@147dda08ca13.ant.amazon.com>
2020-12-29 22:33:06 +0530

    changed install_composer_dependencies.sh - Third Time

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/www/html
 yum install wget -y
+sudo yum install php7.2 -y 
 wget https://getcomposer.org/composer.phar
-php composer.phar update
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php composer-setup.php
