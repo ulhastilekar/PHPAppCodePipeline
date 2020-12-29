8684569f9685345adbb8fe41469b10ccb82ddfef
Tilekar <tilekaru@147dda08ca13.ant.amazon.com>
2020-12-29 23:22:25 +0530

    changed install_dependencies.sh

--- scripts/boot_user_data_script.sh
+++ scripts/boot_user_data_script.sh
@@ -5,7 +5,10 @@ sudo yum install yum-utils -y
 sudo yum-config-manager --enable remi-php72
 sudo yum update -y 
 
-sudo yum install httpd php72 php72-php-mysqlnd php72-php-pecl-memcache php72-php-pecl-apcu php72-php-gd php-php-mbstring -y --skip-broke
+#sudo yum install httpd php72 php72-php-mysqlnd php72-php-pecl-memcache php72-php-pecl-apcu php72-php-gd php-php-mbstring -y --skip-broken
+
+sudo amazon-linux-extras install -y php7.2
+sudo yum install httpd php-mysqlnd php-pecl-memcache php-pecl-apcu php-gd php-mbstring -y --skip-broken
 
 sudo yum install ruby -y
 sudo yum install wget -y
