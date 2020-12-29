5e48738fd7e085142004ef53ad829d722cfbf291
Tilekar <tilekaru@147dda08ca13.ant.amazon.com>
2020-12-29 23:01:38 +0530

    changed install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-if ! [ -x "$(command -v httpd)" ]; then sudo yum install -y httpd >&2;   exit 1; fi # install apache if not already installed
+#if ! [ -x "$(command -v httpd)" ]; then sudo yum install -y httpd >&2;   exit 1; fi # install apache if not already installed
