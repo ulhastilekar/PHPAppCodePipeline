51f11787f7ccb067d5c6cb22648eb303215e7dc6
Tilekar <tilekaru@147dda08ca13.ant.amazon.com>
2020-12-29 23:29:10 +0530

    changed install_composer_dependencies.sh

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html
 yum install wget -y
-#sudo yum install php7.2 -y 
+sudo amazon-linux-extras install -y php7.2
 wget https://getcomposer.org/composer.phar
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
